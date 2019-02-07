8348e7c597c6efc09220b0e8ffa6f3a5a160c459
Aref <rf.gholmi@yahoo.com>
2019-02-07 20:43:48 +0330

    --

---
+++ src/main/java/com/rfgholami/samples/SpringBootDockerSampleApplication.java
@@ -0,0 +1,27 @@
+package com.rfgholami.samples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class SpringBootDockerSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDockerSampleApplication.class, args);
+    }
+
+}
+
+@RequestMapping(value = "/")
+@RestController
+class HelloController {
+
+    @GetMapping(value = "hello")
+    public String sayHello() {
+        return "Hello";
+    }
+
+}
